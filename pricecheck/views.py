08a011ea00b889dbbae8a2931db9eb8881969a9e
musindmitriy <dmitry.musin@phystech.edu>
2017-12-20 13:35:29 +0300

    Added simple chart

--- pricecheck/views.py
+++ pricecheck/views.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime, json
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -15,5 +15,11 @@ def main(request, id):
     start_from = datetime.date.today() - datetime.timedelta(days=7)
     price_last = Price.objects.filter(item=item,
                                       date__gte=start_from).order_by("date")
+    js = []
+    for price in price_last:
+        js.append(
+            ['new Date("{}T00:00")'.format(price.date.isoformat()), price.price]
+        )
+    js = str(js).replace("'", "")
     return render(request, "pricecheck/main.html",
-                  {"item": item, "price": price_last})
+                  {"item": item, "price": js})
